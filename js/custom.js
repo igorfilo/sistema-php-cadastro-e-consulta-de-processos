8d684cae9e3bc708010693a6150748c76087f05d
Igor F <71834181+igorfilo@users.noreply.github.com>
2022-01-09 12:12:47 -0400

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,100 @@
+﻿/* Atribui ao evento submit do formulário a função de validação de dados */
+var form = document.getElementById("form-contato");
+if (form != null && form.addEventListener) {                   
+    form.addEventListener("submit", validaCadastro);
+} else if (form != null && form.attachEvent) {                  
+    form.attachEvent("onsubmit", validaCadastro);
+}
+
+
+/* Atribui ao evento click do link de exclusão na página de consulta a função confirmaExclusao */
+var linkExclusao = document.querySelectorAll(".link_exclusao");
+if (linkExclusao != null) { 
+	for ( var i = 0; i < linkExclusao.length; i++ ) {
+		(function(i){
+			var id_processo = linkExclusao[i].getAttribute('rel');
+
+			if (linkExclusao[i].addEventListener){
+		    	linkExclusao[i].addEventListener("click", function(){confirmaExclusao(id_processo);});
+			}else if (linkExclusao[i].attachEvent) { 
+				linkExclusao[i].attachEvent("onclick", function(){confirmaExclusao(id_processo);});
+			}
+		})(i);
+	}
+}
+
+/* Função para validar os dados antes da submissão dos dados */
+function validaCadastro(evt){
+
+	var status = document.getElementById('status');
+	var filtro = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+	var contErro = 0;
+
+	/* Validação do campo status */
+	caixa_status = document.querySelector('.msg-status');
+	if(status.value == ""){
+		caixa_status.innerHTML = "Favor preencher os campos corretamente";
+		caixa_status.style.display = 'block';
+		contErro += 1;
+	}else{
+		caixa_status.style.display = 'none';
+	}
+
+	if(contErro > 0){
+		evt.preventDefault();
+	}
+}
+
+
+/* Função para exibir um alert confirmando a exclusão do registro*/
+function confirmaExclusao(id){
+	retorno = confirm("Deseja excluir esse Registro?")
+
+	if (retorno){
+
+	    //Cria um formulário
+	    var formulario = document.createElement("form");
+	    formulario.action = "acao_processo.php";
+	    formulario.method = "post";
+
+		// Cria os inputs e adiciona ao formulário
+	    var inputAcao = document.createElement("input");
+	    inputAcao.type = "hidden";
+	    inputAcao.value = "excluir";
+	    inputAcao.name = "acao_processo";
+	    formulario.appendChild(inputAcao); 
+
+	    var inputId = document.createElement("input");
+	    inputId.type = "hidden";
+	    inputId.value = id;
+	    inputId.name = "id";
+	    formulario.appendChild(inputId);
+	
+	
+		//formulário protocolo -------------------------
+	    var formulario = document.createElement("form");
+	    formulario.action = "acao_processo.php";
+	    formulario.method = "post";
+
+		// Cria os inputs e adiciona ao formulário
+	    var inputAcao = document.createElement("input");
+	    inputAcao.type = "hidden";
+	    inputAcao.value = "excluir";
+	    inputAcao.name = "acao_processo";
+	    formulario.appendChild(inputAcao); 
+
+	    var inputId = document.createElement("input");
+	    inputId.type = "hidden";
+	    inputId.value = id;
+	    inputId.name = "id";
+	    formulario.appendChild(inputId);
+
+
+
+	    //Adiciona o formulário ao corpo do documento
+	    document.body.appendChild(formulario);
+
+	    //Envia o formulário
+	    formulario.submit();
+	}
+}
