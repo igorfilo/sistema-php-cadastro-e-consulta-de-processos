9e8ca8e84dfee06848e2a47d50a015bd223de657
Igor F <71834181+igorfilo@users.noreply.github.com>
2024-02-12 12:24:00 -0400

    Add files via upload

---
+++ js/custom_usuarios.js
@@ -0,0 +1,71 @@
+﻿
+/* Atribui ao evento click do link de exclusão na página de consulta a função confirmaExclusao */
+var linkExclusao = document.querySelectorAll(".link_exclusao");
+if (linkExclusao != null) { 
+	for ( var i = 0; i < linkExclusao.length; i++ ) {
+		(function(i){
+			var id_processo = linkExclusao[i].getAttribute('rel');
+
+			if (linkExclusao[i].addEventListener){
+		    	linkExclusao[i].addEventListener("click", function(){confirmaExclusao(id_processo);});
+			}else if (linkExclusao[i].attachEvent) { 
+				linkExclusao[i].attachEvent("onclick", function(){confirmaExclusao(id_processo);});
+			}
+		})(i);
+	}
+}
+
+
+
+/* Função para exibir um alert confirmando a exclusão do registro*/
+function confirmaExclusao(id){
+	retorno = confirm("Deseja excluir esse Registro?")
+
+	if (retorno){
+
+	    //Cria um formulário
+	    var formulario = document.createElement("form");
+	    formulario.action = "acao_usuarios.php";
+	    formulario.method = "post";
+
+		// Cria os inputs e adiciona ao formulário
+	    var inputAcao = document.createElement("input");
+	    inputAcao.type = "hidden";
+	    inputAcao.value = "excluir";
+	    inputAcao.name = "acao_usuarios";
+	    formulario.appendChild(inputAcao); 
+
+	    var inputId = document.createElement("input");
+	    inputId.type = "hidden";
+	    inputId.value = id;
+	    inputId.name = "id";
+	    formulario.appendChild(inputId);
+	
+	
+		//formulário protocolo -------------------------
+	    var formulario = document.createElement("form");
+	    formulario.action = "acao_usuarios.php";
+	    formulario.method = "post";
+
+		// Cria os inputs e adiciona ao formulário
+	    var inputAcao = document.createElement("input");
+	    inputAcao.type = "hidden";
+	    inputAcao.value = "excluir";
+	    inputAcao.name = "acao_usuarios";
+	    formulario.appendChild(inputAcao); 
+
+	    var inputId = document.createElement("input");
+	    inputId.type = "hidden";
+	    inputId.value = id;
+	    inputId.name = "id";
+	    formulario.appendChild(inputId);
+
+
+
+	    //Adiciona o formulário ao corpo do documento
+	    document.body.appendChild(formulario);
+
+	    //Envia o formulário
+	    formulario.submit();
+	}
+}
